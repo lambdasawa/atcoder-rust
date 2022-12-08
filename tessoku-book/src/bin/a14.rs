63f56fd544789a0299149b61a97da73685cdf934
Tsubasa Irisawa <lambdasawa@gmail.com>
2022-12-09 08:58:29 +0900

    solve

--- tessoku-book/src/bin/a14.rs
+++ tessoku-book/src/bin/a14.rs
@@ -2,8 +2,62 @@ use proconio::input;
 
 fn main() {
     input! {
-        S: String,
+        N: usize,
+        K: usize,
+        A: [usize; N],
+        B: [usize; N],
+        C: [usize; N],
+        D: [usize; N],
     }
 
-    println!("{}", S);
+    let mut P = vec![0; N * N];
+    let mut Q = vec![0; N * N];
+    for i in 0..N {
+        for j in 0..N {
+            P[i * N + j] = A[i] + B[j];
+            Q[i * N + j] = C[i] + D[j];
+        }
+    }
+
+    P.sort();
+    Q.sort();
+
+    for p in P {
+        if binary_search(&Q, K - p).is_some() {
+            println!("{}", "Yes");
+            return;
+        };
+    }
+
+    println!("{}", "No");
+}
+
+fn binary_search(A: &Vec<usize>, x: usize) -> Option<usize> {
+    let len = A.len();
+
+    let mut l = 0;
+
+    let mut r = len - 1;
+
+    while l <= r {
+        let m = (l + r) / 2;
+        let a = A[m];
+        if a < x {
+            if m >= len {
+                return None;
+            }
+            l = m + 1;
+        }
+        if a > x {
+            if m <= 0 {
+                return None;
+            }
+            r = m - 1;
+        }
+        if a == x {
+            return Some(m);
+        }
+    }
+
+    return None;
 }

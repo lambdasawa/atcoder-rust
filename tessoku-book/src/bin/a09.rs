9f3cb471a0881aec47984113df4986f417f34e36
Tsubasa Irisawa <lambdasawa@gmail.com>
2022-12-08 09:48:29 +0900

    solve

--- tessoku-book/src/bin/a09.rs
+++ tessoku-book/src/bin/a09.rs
@@ -2,8 +2,49 @@ use proconio::input;
 
 fn main() {
     input! {
-        S: String,
+        H: usize,
+        W: usize,
+        N: usize,
+        ABCD: [(usize,usize,usize,usize); N],
     }
 
-    println!("{}", S);
+    let mut accum = vec![vec![0isize; W + 9]; H + 9];
+
+    for (low_h, low_w, high_h, high_w) in ABCD {
+        accum[low_h][low_w] += 1;
+        accum[low_h][high_w + 1] -= 1;
+        accum[high_h + 1][low_w] -= 1;
+        accum[high_h + 1][high_w + 1] += 1;
+    }
+
+    for h in 1..=H {
+        for w in 1..=W {
+            accum[h][w] = accum[h][w - 1] + accum[h][w];
+        }
+    }
+    for h in 1..=H {
+        for w in 1..=W {
+            accum[h][w] = accum[h - 1][w] + accum[h][w];
+        }
+    }
+
+    for h in 1..=H {
+        println!(
+            "{}",
+            accum[h][1..=W]
+                .iter()
+                .map(|n| n.to_string())
+                .collect::<Vec<String>>()
+                .join(" ")
+        );
+    }
+}
+
+fn print_table(table: &Vec<Vec<isize>>) {
+    for line in table {
+        for n in line {
+            print!("{:4} ", n);
+        }
+        println!("")
+    }
 }
--- tessoku-book/src/bin/a10.rs
+++ tessoku-book/src/bin/a10.rs
@@ -2,8 +2,28 @@ use proconio::input;
 
 fn main() {
     input! {
-        S: String,
+        N: usize,
+        A: [usize; N],
+        D: usize,
+        LR: [(usize, usize); D],
     }
 
-    println!("{}", S);
+    let mut left_to_right = vec![0; N];
+    left_to_right[0] = A[0];
+    for i in 1..N {
+        left_to_right[i] = left_to_right[i - 1].max(A[i]);
+    }
+
+    let mut right_to_left = vec![0; N];
+    right_to_left[N - 1] = A[N - 1];
+    for i in (0..N - 1).rev() {
+        right_to_left[i] = right_to_left[i + 1].max(A[i]);
+    }
+
+    for (L, R) in LR {
+        let li = L - 1;
+        let ri = R - 1;
+
+        println!("{}", left_to_right[li - 1].max(right_to_left[ri + 1]));
+    }
 }

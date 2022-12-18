1dc2d815becc1510b5d8844e0fcf76104a03e089
Tsubasa Irisawa <lambdasawa@gmail.com>
2022-12-18 18:31:08 +0900

    refactor

--- abc282/src/bin/d.rs
+++ abc282/src/bin/d.rs
@@ -1,81 +1,96 @@
+use std::collections::HashMap;
+
 use proconio::{input, marker::Chars};
 
-static COLOR_NONE: usize = 0;
-static COLOR_RED: usize = 1;
-static COLOR_BLUE: usize = 2;
+const COLOR_NONE: usize = 0;
+const COLOR_RED: usize = 1;
+const COLOR_BLUE: usize = 2;
+
+struct Solver {
+    colors: Vec<usize>,
+    color_counts: Vec<usize>,
+}
+
+impl Solver {
+    fn solve(&mut self) {
+        input! {
+            N: usize, // node count
+            M: usize, // edge count
+            G: [(usize, usize); M],
+        }
+
+        let mut graph = vec![vec![]; N + 9];
+        for (u, v) in G {
+            graph[u].push(v);
+            graph[v].push(u);
+        }
+
+        self.colors = vec![COLOR_NONE; N + 9];
+
+        let mut invalid_count = 0;
+
+        for i in 1..=N {
+            if self.colors[i] != COLOR_NONE {
+                continue; // この連結成分は既にチェック済み
+            }
+
+            // この連結成分内に出現する色のカウント
+            self.color_counts = vec![0; 3];
+
+            // どこかの連結成分が二部グラフじゃなかったら、全体が二部グラフじゃないので諦める
+            if self.dfs(&graph, i, COLOR_RED) == false {
+                println!("0");
+                return;
+            }
+
+            for count in self.color_counts.iter() {
+                // 同一連結成分内の同色のノード同士に辺をつけると二部グラフにならない。
+                // そのため、同一連結成分内の同色のノード同士を2個選ぶ組み合わせをカウントする。
+                invalid_count += self.combination_count_x_2(*count);
+            }
+        }
+
+        let all_count = self.combination_count_x_2(N);
+
+        // 存在しうる全ての辺から、既存の辺と、二部グラフにならなくなってしまう辺の数を引く。
+        println!("{}", all_count - M - invalid_count);
+    }
+
+    fn combination_count_x_2(&self, x: usize) -> usize {
+        if x <= 1 {
+            return 0;
+        }
+        x * (x - 1) / 2
+    }
+
+    fn dfs(&mut self, graph: &Vec<Vec<usize>>, v: usize, c: usize) -> bool {
+        if self.colors[v] != COLOR_NONE {
+            return self.colors[v] == c; // 既に着色済みなら、同じ色かどうかチェック。違ったら二部グラフじゃない。
+        }
+
+        self.colors[v] = c; // 着色
+        self.color_counts[c] += 1; // 連結グラフ内で色をカウント
+
+        for n in graph[v].iter() {
+            let next_color = match c {
+                COLOR_RED => COLOR_BLUE,
+                COLOR_BLUE => COLOR_RED,
+                _ => panic!("invalid color"),
+            };
+
+            if !self.dfs(graph, *n, next_color) {
+                return false;
+            }
+        }
+
+        true
+    }
+}
 
 fn main() {
-    input! {
-        N: usize, // node count
-        M: usize, // edge count
-        G: [(usize, usize); M],
-    }
-
-    let mut graph = vec![vec![]; N + 9];
-    for (u, v) in G {
-        graph[u].push(v);
-        graph[v].push(u);
-    }
-
-    let mut colors = vec![COLOR_NONE; N + 9];
-    let mut color_counts = vec![0; 3];
-
-    let mut invalid_count = 0;
-
-    for i in 1..=N {
-        if colors[i] != COLOR_NONE {
-            continue;
-        }
-
-        color_counts = vec![0; 3];
-
-        if dfs(&graph, &mut colors, &mut color_counts, i, COLOR_RED) == false {
-            println!("0");
-            return;
-        }
-
-        for count in color_counts {
-            invalid_count += combination_count_x_2(count);
-        }
-    }
-
-    let all_count = combination_count_x_2(N);
-
-    println!("{}", all_count - M - invalid_count);
-}
-
-fn combination_count_x_2(x: usize) -> usize {
-    if x <= 1 {
-        return 0;
-    }
-    x * (x - 1) / 2
-}
-
-fn dfs(
-    graph: &Vec<Vec<usize>>,
-    colors: &mut Vec<usize>,
-    color_counts: &mut Vec<usize>,
-    v: usize,
-    c: usize,
-) -> bool {
-    if colors[v] != COLOR_NONE {
-        return colors[v] == c; // 既に着色済みなら、同じ色かどうかチェック。違ったら二部グラフじゃない。
-    }
-
-    colors[v] = c; // 着色
-    color_counts[c] += 1; // 連結グラフ内で色をカウント
-
-    for n in graph[v].iter() {
-        let next_color = if c == COLOR_RED {
-            COLOR_BLUE
-        } else {
-            COLOR_RED
-        };
-
-        if !dfs(graph, colors, color_counts, *n, next_color) {
-            return false;
-        }
-    }
-
-    true
+    let mut solver = Solver {
+        colors: vec![],
+        color_counts: vec![],
+    };
+    solver.solve();
 }
